38dc1434b9e9552ebc81cba2186756ce51e65a87
esteban-serrano <enserran@uc.cl>
2012-06-30 17:58:39 -0400

    newConHandler con metodos pa responder

--- src/tareaiiiredes/NewConHandler.java
+++ src/tareaiiiredes/NewConHandler.java
@@ -10,47 +10,108 @@ import java.net.URLDecoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.ResultSet;
+
 /**
  *
  * @author Rodrigo
  */
 public class NewConHandler implements Runnable {
-    
-    
-        Socket m_connection = null;
-	int m_clientID = -1;
-	boolean m_bRunThread = true;
 
-	NewConHandler(Socket NewCon, int clientID) throws IOException {
-		m_connection = NewCon;
-		m_clientID = clientID;
-		System.out.println("server, client id is: " + m_clientID);
-	  
+    Socket m_connection = null;
+    int m_clientID = -1;
+    boolean m_bRunThread = true;
+
+    NewConHandler(Socket NewCon, int clientID) throws IOException {
+        m_connection = NewCon;
+        m_clientID = clientID;
+        System.out.println("server, client id is: " + m_clientID);
+
+    }
+    
+    private ResultSet getQueryResults(String host, String query)
+    {
+        ResultSet rs = null;
+        QueryExecution qexec = QueryExecutionFactory.sparqlService(host, query);
+        try
+        {
+            rs = qexec.execSelect();
         }
+        catch(Exception e)
+        {
+            System.out.println("Excepción en ResultSet QueryRemoteSparql.ResultSet en NewConHandler, client id: " + m_clientID + ":");
+            System.out.printf(e.toString());
+        }
+        return rs;
+    }
+    
+    private void sendHTTP200OKResponse(BufferedWriter out, String body)
+    {
         
-        @Override
-	public void run() {
+    }
+    
+    private void sendHTTP500InternalServerErrorResponse(BufferedWriter out)
+    {
+        try
+        {
+            out.write("HTTP/1.1 500 Internal Server Error");
+            out.flush();
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+    
+    
+    @Override
+    public void run() {
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new InputStreamReader(m_connection.getInputStream()));
-            OutputStream out = new BufferedOutputStream(m_connection.getOutputStream());
-            PrintStream pout = new PrintStream(out);
+            in               =  new BufferedReader(new InputStreamReader(m_connection.getInputStream()));
+            //OutputStream out =  new BufferedOutputStream(m_connection.getOutputStream());
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(m_connection.getOutputStream(), "UTF8"));
+            //PrintStream pout =  new PrintStream(out);
+            
             // read first line of request (ignore the rest)
             String request = in.readLine();
-            if (request != null)
+            
+            if (request.substring(0, 3).equals("POST"))
             {
-             int i = 0;
-            while (true) {                    
-               //Read the http data message
-                String misc = in.readLine();
-                if(i==2)
-                    misc = URLDecoder.decode(misc, "UTF-8");
-                System.out.println(misc);
-                i++;
-                
-                if (misc==null || misc.length()==0)
-                    break;
-            }   
+                int i = 0;
+                while (true) {
+                    //Read the http data message
+                    String misc = in.readLine();
+                    if (i == 2) {
+                        // Según el número de encabezados definidos en nuestro cliente,
+                        // cuando i = 2, misc corresponde al contenido del mensaje,
+                        // en este caso, la consulta SPARQL.
+                        misc = URLDecoder.decode(misc, "UTF-8");
+                        ResultSet r = this.getQueryResults("http://dbpedia.org/sparql", misc);
+                        
+                        if(r == null)
+                        {
+                            this.sendHTTP500InternalServerErrorResponse(out);
+                            break;
+                        }
+                        else
+                        {
+                            this.sendHTTP200OKResponse(out, r.toString());
+                        }
+                        
+                    }
+                    System.out.println("[i = " + i + "]\t" + misc);
+                    i++;
+
+                    if (misc == null || misc.length() == 0) {
+                        break;
+                    }
+                }
+            }
+            else if (request.substring(0, 2).equals("GET"))
+            {
                 
             }
         } catch (IOException ex) {
@@ -62,8 +123,5 @@ public class NewConHandler implements Runnable {
                 Logger.getLogger(NewConHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        }
-                
-                
-    
+    }
 }
